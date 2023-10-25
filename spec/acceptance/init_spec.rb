3976544441239efa4f96443635e2f6615be9bcf9
Tim Meusel <tim@bastelfreak.de>
2023-10-25 19:14:20 +0200

    Enhance acceptance tests

--- spec/acceptance/init_spec.rb
+++ spec/acceptance/init_spec.rb
@@ -11,5 +11,23 @@ describe 'tang class' do
         PUPPET
       end
     end
+
+    describe package('tang') do
+      it { is_expected.to be_installed }
+    end
+
+    describe service('tangd.socket') do
+      it { is_expected.to be_running }
+      it { is_expected.to be_enabled }
+    end
+
+    describe port(7500) do
+      it { is_expected.to be_listening }
+    end
+
+    describe command('curl --silent http://127.0.0.1:7500/adv') do
+      its(:stdout) { is_expected.to match %r{payload} }
+      its(:exit_status) { is_expected.to eq 0 }
+    end
   end
 end
